684c7cad2d0aeb399900fbf631f2db05ef075afe
Ryan Semmler <semmra7@gmail.com>
2017-03-23 17:03:08 -0400

    added migrations

--- cl_project/cl_app/models.py
+++ cl_project/cl_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Posting(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.CharField(max_length=1500)
+    image = models.CharField(max_length=100)
+    # category = models.ForeignKey(null=True)
+    city = models.CharField(max_length=20)
+    # user = models.ForeignKey(null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    # parent_category = models.ForeignKey(null=True)
